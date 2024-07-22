2db8a72dd0ea3fec0e3919ae4e1b94e10cf50377
Vsevolod Shmelkin <Schmel924@gmail.com>
2024-07-22 20:53:02 +0300

    @ is move

--- main.lua
+++ main.lua
@@ -1,9 +1,16 @@
+require ("Keypod")
+
 function love.load(arg)
   if arg[#arg] == "-debug" then require("mobdebug").start() end
  img = love.graphics.newImage("dejavu10x10_gs_tc.png")
  h = img:getHeight()
  w = img:getWidth()
  quad = love.graphics.newQuad( 0, 10, 10, 10, w,h)
+ local _w,_h, fl = love.window.getMode()
+ love.window.setTitle("Yet Another Roguelike Tutorial" )
+ love.window.setMode(500,500,fl)
+ player_x = 250
+ player_y = 250
 end
 
 function love.update(dt)
@@ -13,5 +20,5 @@ end
 
 
 function love.draw()
-	love.graphics.draw (img,quad, 50, 50)
+	love.graphics.draw (img,quad, player_x, player_y)
 end
