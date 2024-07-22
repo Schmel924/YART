108de8e26b564a9e050f20a032e4e2a4044a558b
Vsevolod Shmelkin <Schmel924@gmail.com>
2024-07-22 20:30:48 +0300

    FontFile loaded

--- main.lua
+++ main.lua
@@ -1,14 +1,17 @@
 function love.load(arg)
   if arg[#arg] == "-debug" then require("mobdebug").start() end
-  x = 0
+ img = love.graphics.newImage("dejavu10x10_gs_tc.png")
+ h = img:getHeight()
+ w = img:getWidth()
+ quad = love.graphics.newQuad( 0, 10, 10, 10, w,h)
 end
 
 function love.update(dt)
-x=x+dt
-y = math.ceil(x)
+
 end  
+
+
+
 function love.draw()
-  love.graphics.setColor(20,255,0,255)
-  love.graphics.print("Hello", 100, 100)
-  love.graphics.print(y, 150,150)
+	love.graphics.draw (img,quad, 50, 50)
 end
