b7b142b20a19ba4085226353746c9f724f890f04
Vsevolod Shmelkin <Schmel924@gmail.com>
2024-08-01 13:07:23 +0300

    Bresenham alghoritm implemented on my scale. Now i need to ensure the walkability of corridors

--- gamemap.lua
+++ gamemap.lua
@@ -7,6 +7,68 @@ local function newtile(t, walkable, transparent)
 	return t
 end
 
+--bresenham algorhitm
+local function plotLineLow(x0, y0, x1, y1)
+    local dx = x1 - x0
+    local dy = y1 - y0
+    local yi = 1
+    if dy < 0 then
+        yi = -1
+        dy = -dy
+    end
+    local D = (2 * dy) - dx
+    local y = y0
+    for x = x0 , x1 do
+	Gamemap[x][y]=Floor
+        --plot(x, y)
+        if D > 0 then
+            y = y + yi
+            D = D + (2 * (dy - dx))
+        else
+            D = D + 2*dy
+        end
+	end
+end
+
+local function plotlineHigh(x0, y0, x1, y1)
+    local dx = x1 - x0
+    local dy = y1 - y0
+    local xi = 1
+    if dx < 0 then
+        xi = -1
+        dx = -dx
+    end
+    D = (2 * dx) - dy
+    local x = x0
+    for y = y0 , y1 do
+        -- plot(x, y)
+	Gamemap[x][y]=Floor
+        if D > 0 then
+            x = x + xi
+            D = D + (2 * (dx - dy))
+        else
+            D = D + 2*dx
+        end
+	end
+end
+
+
+local function plotline(x0,y0,x1,y1)
+	if math.abs(y1-y0) < math.abs(x1-x0) then
+		if x0>x1 then
+		plotLineLow(x1,y1,x0,y0)
+	else
+		plotLineLow(x0,y0,x1,y1)
+		end
+	else
+		if y0 > y1 then
+		plotlineHigh(x1,y1,x0,y0)
+	else
+		plotlineHigh(x0,y0,x1,y1)
+		end
+	end
+end
+
 local function CenterRoom(x,y,w,h)
 	local cx = w/2 + x
 	local cy = h/2 + y
@@ -44,12 +106,15 @@ local function GenerateDungeon()
 	local rooms = {}
 	rooms[1] = RectangularRoom(4,4,4,4)
 	rooms[2] = RectangularRoom(8,12,4,4)
-	print(rooms[1].w)
+	rooms[3] = RectangularRoom(17,5,3,10)
 	for k,v in pairs(rooms) do
 		for i = 1,v.w do
 			for j = 1,v.h do
 				Gamemap[v.x+i-1][v.y+j-1] = Floor
 			end
+		end
+		if rooms[k+1] then
+			plotline(rooms[k].cx,rooms[k].cy,rooms[k+1].cx,rooms[k+1].cy)
 		end
 	end
 end
