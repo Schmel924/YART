aff29882032b02ff3b74abf5745d2b35fc85511c
Vsevolod Shmelkin <Schmel924@gmail.com>
2024-08-10 19:08:24 +0300

    Very minimal inventory

--- Keypod.lua
+++ Keypod.lua
@@ -1,10 +1,17 @@
 -- keypod.lua
 function love.keypressed(key, scancode, isrepeat)
+	if Inv_mode then
+	if key == "down" then Selector=Selector+1 end
+	if key == "up" then Selector = Selector - 1 end
+	if (key == "space") then UseInventorySpace(Selector) end
+	else
 	if key == "down" then ActionDirPlayer(Player, 0, 1) end
 	if key == "up" then ActionDirPlayer(Player, 0, -1) end
 	if key == "right" then ActionDirPlayer(Player, 1, 0) end
 	if key == "left" then ActionDirPlayer(Player, -1, 0)end
 	if (key == "space") then ActionDirPlayer(Player, 0,0) end
+	end
 	if key == "escape" then love.event.quit("restart") end
 	if key == 'i' then Toggle_inv_mod() end
+	if key == 'o' then AddBottle() end
 end
--- inventory.lua
+++ inventory.lua
@@ -5,12 +5,49 @@ Inventory = {}
 Inv_mode = false
 
 local inv_lines = 4
+Selector = 1
+
+function AddBottle()
+table.insert(Inventory,{
+name = "bottle",
+hp = 10,
+})
+end
 
 function Show_inventory()
-if Inv_mode then 
+if Inv_mode then
+		love.graphics.setColor(Black)
+		love.graphics.rectangle("fill",1,1,Fontsize*Worldsize,Fontsize*Worldsize/2)
+		love.graphics.setColor(White)
+		love.graphics.rectangle("line", 1,1,Fontsize*Worldsize-1, (Fontsize*Worldsize/2)-1)
+		if Selector < 1 then Selector = inv_lines end
+		if Selector > inv_lines then Selector =1 end
+
+		for i =1, inv_lines, 1 do
+			love.graphics.setColor(White)
+			if i == Selector then
+				love.graphics.setColor(Green)
+			end
+			if Inventory[i] then
+				love.graphics.print(Inventory[i].name,1, (Scale+Fontsize)*i, 0, Scale)
+			else
+			love.graphics.print("[-] Empty", 1, (Scale+Fontsize)*i,  0, Scale)
+			end
+			love.graphics.setColor(White)
+		end
 	end
 end
 
 function Toggle_inv_mod ()
 	Inv_mode = not Inv_mode
+	Selector = 1
+end
+
+function UseInventorySpace(sel)
+	if Inventory[sel] then
+		Player.hp = Player.hp + Inventory[sel].hp
+		table.remove(Inventory,sel)
+	else
+		GrabLog("Pockets are empty")
+	end
 end
